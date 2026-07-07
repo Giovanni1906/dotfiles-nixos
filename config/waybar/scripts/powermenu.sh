7bfdf91c00d616480e97bc89e339dfbd9ecefc72
Giovanni1906 <jeff-1906@outlook.com>
2026-07-06 23:03:43 -0500

    fix: mejora tamaño de script powermenu

--- config/waybar/scripts/powermenu.sh
+++ config/waybar/scripts/powermenu.sh
@@ -8,10 +8,10 @@ TEXTO_SELECT="#cdd6f4"
 
 # Quitamos el -p "Sistema:" y apagamos el inputbar
 chosen=$(printf " Apagar\n Reiniciar\n Suspender\n Cerrar Sesión" | rofi -dmenu -i \
--theme-str "window { location: center; anchor: center; width: 15%; border: 2px; border-color: $BORDE; border-radius: 10px; background-color: $FONDO; padding: 15px; }" \
+-theme-str "window { location: center; anchor: center; width: 20%; border: 2px; border-color: $BORDE; border-radius: 10px; background-color: $FONDO; padding: 15px; }" \
 -theme-str "inputbar { enabled: false; }" \
 -theme-str "listview { lines: 4; scrollbar: false; background-color: transparent; margin: 0; }" \
--theme-str "element { padding: 8px; border-radius: 5px; background-color: transparent; }" \
+-theme-str "element { padding: 15px 10px; border-radius: 5px; background-color: transparent; }" \
 -theme-str "element normal.normal, element alternate.normal { background-color: transparent; }" \
 -theme-str "element selected.normal { background-color: $FONDO_SELECT; border: 1px; border-color: $MORADO; }" \
 -theme-str "element-text { text-color: $TEXTO; background-color: transparent; }" \
